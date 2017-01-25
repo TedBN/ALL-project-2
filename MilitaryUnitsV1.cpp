3b94836f41adc261d8e2fffac25838c52627a055
TedBN <atanaso5@uni.coventry.ac.uk>
2017-01-25 15:44:43 +0000

    Creation of basic unit classes
    The text of a .cpp file that contains the classes for the game's combat units plus their basic attributes and files.
    A test is included in the main function of this file. In the upcoming versions I plan to add an attack function and
    a system that determines whether a unit is weak or strong against another.

---
+++ MilitaryUnitsV1.cpp
@@ -0,0 +1,57 @@
+// MilitaryUnits.cpp : Defines the class for military units, along with it's functions and children.
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class MilitaryUnit {
+	public:
+		int attack, health, speed, range, energyCost, metalCost, oilCost, manpowerCost, buildCost;
+		void set_stats(int, int, int);
+		void set_range(int);
+		void set_resourceCost(int,int,int,int);
+		void set_buildCost(int);
+};
+
+void MilitaryUnit::set_stats(int a, int h, int s) {
+	attack = a;
+	health = h;
+	speed = s;
+};
+
+void MilitaryUnit::set_range(int r) {
+	range = r;
+};
+
+void MilitaryUnit::set_resourceCost(int e, int m, int o, int t) {
+	energyCost = e;
+	metalCost = m;
+	oilCost = o;
+	manpowerCost = t;
+};
+
+void MilitaryUnit::set_buildCost(int c) {
+	buildCost = c;
+};
+
+class LandUnit : public MilitaryUnit {
+}infantry, engineers, tanks, helicopters, artillery, antiair, antitank;
+
+class AirUnit : public MilitaryUnit {
+}fighter, bomber;
+
+class NavalUnit : public MilitaryUnit {
+}submarine, destroyer, cruiser, heavyCruiser, battleship, aircraftCarrier;
+
+int main()
+{
+	cout << "Function test:\n";
+	LandUnit infantry;
+	infantry.set_stats(75,750,2);
+	infantry.set_range(0);
+	infantry.set_resourceCost(50,150,0,8);
+	infantry.set_buildCost(800);
+	cout << "Combat stats: " << infantry.attack << " attack, " << infantry.health << " HP, " << infantry.speed << " speed\n";
+	cout << "Resource cost: " << infantry.energyCost << " energy, " << infantry.metalCost << " metal, " << infantry.oilCost << " oil, " << infantry.manpowerCost << " manpower, " << infantry.buildCost << " production to build\n";
+    return 0;
+};
