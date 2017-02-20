cfd379871c56e56601e35ce59c6bb438edaf2173
TedBN <atanaso5@uni.coventry.ac.uk>
2017-02-20 09:43:42 +0000

    Delete diplomacyRequest.h

--- diplomacyRequest.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Oskars Pozdnakovs on 25/01/2017.
-//
-
-#ifndef EUROPE_AT_WAR_DIPLOMACYREQUEST_H
-#define EUROPE_AT_WAR_DIPLOMACYREQUEST_H
-
-#endif //EUROPE_AT_WAR_DIPLOMACYREQUEST_H
